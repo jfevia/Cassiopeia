30f5d746a433ddcabd12d365d6d43393326838ca
Jesus Fernandez <jfevia@gmail.com>
2017-01-04 07:29:59 +0200

    Replace Avalon dock and add Input Gesture Command for torrent file

---
+++ src/Cassiopeia/Models/InputGestureCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GalaSoft.MvvmLight.Command;
+
+namespace Cassiopeia.Models
+{
+    internal class InputGestureCommand : RelayCommand
+    {
+        public InputGestureCommand(Action execute, Func<bool> canExecute, string inputGestureText)
+            : base(execute, canExecute)
+        {
+            InputGestureText = inputGestureText;
+        }
+
+        public InputGestureCommand(Action execute, string inputGestureText) : base(execute)
+        {
+            InputGestureText = inputGestureText;
+        }
+
+        public string InputGestureText { get; }
+    }
+}
--- src/Cassiopeia/ViewModels/MainViewModel.cs
+++ src/Cassiopeia/ViewModels/MainViewModel.cs
@@ -1,7 +1,13 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Input;
+using Cassiopeia.BitTorrent;
+using Cassiopeia.Models;
 using Cassiopeia.Services;
 using Cassiopeia.Views;
 using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
 
 namespace Cassiopeia.ViewModels
 {
@@ -9,14 +15,28 @@ namespace Cassiopeia.ViewModels
     {
         public MainViewModel()
         {
-            AddTorrentCommand = new RelayCommand(ShowAddTorrentCommandWindow);
-        }
-
-        private void ShowAddTorrentCommandWindow()
-        {
-            WindowService.ShowWindow<AddTorrentWindow>();
+            AddTorrentCommand = new RelayCommand(ShowAddTorrentWindow);
+            OpenFileCommand = new InputGestureCommand(ShowOpenFileDialog, "Ctrl+O");
         }
 
         public ICommand AddTorrentCommand { get; }
+        public InputGestureCommand OpenFileCommand { get; }
+
+        public void ShowOpenFileDialog()
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                BEncode.Decode(File.ReadAllBytes(openFileDialog.FileName));
+                Console.WriteLine($"File read completed in {stopwatch.Elapsed}");
+            }
+        }
+
+        private void ShowAddTorrentWindow()
+        {
+            WindowService.ShowWindow<AddTorrentWindow>();
+        }
     }
 }
