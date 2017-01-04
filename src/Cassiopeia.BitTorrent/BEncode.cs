f771f5c9dc023e8374374412b4c3246bfa36c5fa
Jesus Fernandez <jfevia@gmail.com>
2017-01-04 07:28:51 +0200

    Extract static methods from BEncodedValue class

---
+++ src/Cassiopeia.BitTorrent/BEncode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Cassiopeia.BitTorrent
+{
+    public class BEncode
+    {
+        public static BEncodedValue Decode(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+
+            using (var stream = new BitTorrentReader(new MemoryStream(data)))
+            {
+                return Decode(stream);
+            }
+        }
+
+        public static BEncodedValue Decode(byte[] buffer, int offset, int length)
+        {
+            if (buffer == null)
+                throw new ArgumentNullException(nameof(buffer));
+
+            if (offset < 0 || length < 0)
+                throw new IndexOutOfRangeException("Neither offset or length can be less than zero");
+
+            if (offset > buffer.Length - length)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            using (var reader = new BitTorrentReader(new MemoryStream(buffer, offset, length)))
+            {
+                return Decode(reader);
+            }
+        }
+
+        public static BEncodedValue Decode(Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
+            return BEncodedValue.Decode(new BitTorrentReader(stream));
+        }
+
+        public static T Decode<T>(byte[] data) where T : BEncodedValue
+        {
+            return (T) Decode(data);
+        }
+
+        public static T Decode<T>(byte[] buffer, int offset, int length) where T : BEncodedValue
+        {
+            return (T) Decode(buffer, offset, length);
+        }
+
+        public static T Decode<T>(Stream stream) where T : BEncodedValue
+        {
+            return (T) Decode(stream);
+        }
+
+        public static T Decode<T>(BitTorrentReader reader) where T : BEncodedValue
+        {
+            return (T) Decode(reader);
+        }
+    }
+}
--- src/Cassiopeia.BitTorrent/BEncodedValue.cs
+++ src/Cassiopeia.BitTorrent/BEncodedValue.cs
@@ -42,43 +42,7 @@ namespace Cassiopeia.BitTorrent
         public T Clone<T>(T value)
             where T : BEncodedValue
         {
-            return (T) Decode(value.Encode());
-        }
-
-        public static BEncodedValue Decode(byte[] data)
-        {
-            if (data == null)
-                throw new ArgumentNullException(nameof(data));
-
-            using (var stream = new BitTorrentReader(new MemoryStream(data)))
-            {
-                return Decode(stream);
-            }
-        }
-
-        public static BEncodedValue Decode(byte[] buffer, int offset, int length)
-        {
-            if (buffer == null)
-                throw new ArgumentNullException(nameof(buffer));
-
-            if (offset < 0 || length < 0)
-                throw new IndexOutOfRangeException("Neither offset or length can be less than zero");
-
-            if (offset > buffer.Length - length)
-                throw new ArgumentOutOfRangeException(nameof(length));
-
-            using (var reader = new BitTorrentReader(new MemoryStream(buffer, offset, length)))
-            {
-                return Decode(reader);
-            }
-        }
-
-        public static BEncodedValue Decode(Stream stream)
-        {
-            if (stream == null)
-                throw new ArgumentNullException(nameof(stream));
-
-            return Decode(new BitTorrentReader(stream));
+            return (T) BEncode.Decode(value.Encode());
         }
 
         public static BEncodedValue Decode(BitTorrentReader reader)
@@ -120,27 +84,6 @@ namespace Cassiopeia.BitTorrent
 
             data.Decode();
             return data;
-        }
-
-        public static T Decode<T>(byte[] data) where T : BEncodedValue
-        {
-            return (T) Decode(data);
-        }
-
-        public static T Decode<T>(byte[] buffer, int offset, int length) where T : BEncodedValue
-        {
-            return (T) Decode(buffer, offset, length);
-        }
-
-        public static T Decode<T>(Stream stream) where T : BEncodedValue
-        {
-            return (T) Decode(stream);
-        }
-
-
-        public static T Decode<T>(BitTorrentReader reader) where T : BEncodedValue
-        {
-            return (T) Decode(reader);
         }
     }
 }
