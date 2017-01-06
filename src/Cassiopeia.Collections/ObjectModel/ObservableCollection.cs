9f018f0ba603c4079c9aa5b0c4b638902d4d6fb2
Jesus Fernandez <jfevia@gmail.com>
2017-01-06 21:33:45 +0200

    Add 'Range' methods to ObservableCollection

--- src/Cassiopeia.Collections/ObjectModel/ObservableCollection.cs
+++ src/Cassiopeia.Collections/ObjectModel/ObservableCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 
@@ -6,6 +7,20 @@ namespace Cassiopeia.Collections.ObjectModel
 {
     public class ObservableCollection<T> : System.Collections.ObjectModel.ObservableCollection<T>
     {
+        public ObservableCollection()
+        {
+        }
+
+        public ObservableCollection(IEnumerable<T> collection) : base
+            (collection)
+        {
+        }
+
+        public ObservableCollection(List<T> collection) : base
+            (collection)
+        {
+        }
+
         public void RemoveRange(int index, int count)
         {
             CheckReentrancy();
@@ -33,6 +48,43 @@ namespace Cassiopeia.Collections.ObjectModel
         private void OnPropertyChanged(string propertyName)
         {
             OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            CheckReentrancy();
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            foreach (var i in collection) Items.Add(i);
+            OnReset();
+        }
+
+        public void RemoveRange(IEnumerable<T> collection)
+        {
+            CheckReentrancy();
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            foreach (var i in collection) Items.Remove(i);
+            OnReset();
+        }
+
+        public void Replace(T item)
+        {
+            CheckReentrancy();
+            ReplaceRange(new[] {item});
+        }
+
+        public void ReplaceRange(IEnumerable<T> collection)
+        {
+            CheckReentrancy();
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            Items.Clear();
+            foreach (var i in collection) Items.Add(i);
+            OnReset();
         }
     }
 }
