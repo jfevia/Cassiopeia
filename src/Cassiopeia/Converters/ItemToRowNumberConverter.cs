bf1393a399d5a1a845770d2302739c166bb5c393
Jesus Fernandez <jfevia@gmail.com>
2017-01-06 21:37:44 +0200

    Update UI

---
+++ src/Cassiopeia/Converters/ItemToRowNumberConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cassiopeia.Converters
+{
+    internal class ItemToRowNumberConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length != 2)
+                return null;
+
+            if (values[0] == null || values[1] == null)
+                throw new ArgumentNullException(nameof(values));
+
+            if (!(values[1] is IList))
+                throw new ArgumentException(nameof(values));
+
+            return (((IList) values[1]).IndexOf(values[0]) + 1).ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Cassiopeia/ViewModels/MainViewModel.cs
+++ src/Cassiopeia/ViewModels/MainViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.IO;
-using System.Windows.Input;
 using Cassiopeia.BitTorrent;
+using Cassiopeia.Collections.ObjectModel;
 using Cassiopeia.Converters;
 using Cassiopeia.Models;
 using Cassiopeia.Services;
@@ -8,28 +8,66 @@ using Cassiopeia.Views;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using Microsoft.Win32;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace Cassiopeia.ViewModels
 {
     internal class MainViewModel : ViewModelBase
     {
-        private Torrent _torrent;
+        private ObservableCollection<Torrent> _newTorrents;
+        private Torrent _selectedNewTorrent;
+        private ObservableCollection<Torrent> _torrents;
+        private Torrent _selectedTorrent;
 
         public MainViewModel()
         {
-            AddTorrentCommand = new RelayCommand(ShowAddTorrentWindow);
+            NewTorrents = new ObservableCollection<Torrent>();
+            Torrents = new ObservableCollection<Torrent>();
+            Categories = new ObservableCollection<string>();
+            CachedDownloadFolders = new ObservableCollection<string>();
+            CachedCompletedDownloadFolders = new ObservableCollection<string>();
+            AddTorrentsDialogCommand = new InputGestureCommand(ShowAddTorrentWindow, "Ctrl+N");
             OpenFileCommand = new InputGestureCommand(ShowOpenFileDialog, "Ctrl+O");
+            AddTorrentsCommand = new RelayCommand<IDialog>(OnAddTorrents);
         }
 
-        public ICommand AddTorrentCommand { get; }
+        private void OnAddTorrents(IDialog dialog)
+        {
+            Torrents.AddRange(NewTorrents);
+            dialog.CloseDialog();
+        }
+
+        public ObservableCollection<string> CachedCompletedDownloadFolders { get; set; }
+
+        public ObservableCollection<string> CachedDownloadFolders { get; set; }
+
+        public InputGestureCommand AddTorrentsDialogCommand { get; }
+        public RelayCommand<IDialog> AddTorrentsCommand { get; }
         public InputGestureCommand OpenFileCommand { get; }
 
-        public Torrent Torrent
+        public Torrent SelectedTorrent
         {
-            get { return _torrent; }
-            private set { Set(nameof(Torrent), ref _torrent, value); }
+            get { return _selectedTorrent; }
+            set { Set(nameof(SelectedTorrent), ref _selectedTorrent, value); }
+        }
+
+        public ObservableCollection<string> Categories { get; set; }
+
+        public Torrent SelectedNewTorrent
+        {
+            get { return _selectedNewTorrent; }
+            set { Set(nameof(SelectedNewTorrent), ref _selectedNewTorrent, value); }
+        }
+
+        public ObservableCollection<Torrent> NewTorrents
+        {
+            get { return _newTorrents; }
+            set { Set(nameof(NewTorrents), ref _newTorrents, value); }
+        }
+
+        public ObservableCollection<Torrent> Torrents
+        {
+            get { return _torrents; }
+            set { Set(nameof(Torrents), ref _torrents, value); }
         }
 
         public void ShowOpenFileDialog()
@@ -40,21 +78,18 @@ namespace Cassiopeia.ViewModels
             if (openFileDialog.ShowDialog() == true)
             {
                 var bEncode = BEncode.Decode(File.ReadAllBytes(openFileDialog.FileName));
-                //var serializer = new JsonSerializer();
-                //serializer.Formatting = Formatting.Indented;
-                //serializer.NullValueHandling = NullValueHandling.Ignore;
-                //using (var sw = new StreamWriter(@"bencode.txt"))
-                //using (JsonWriter writer = new JsonTextWriter(sw))
-                //{
-                //    serializer.Serialize(writer, bEncode);
-                //}
-                Torrent = TorrentConverter.ConvertFromBEncode(bEncode);
+                var torrent = TorrentConverter.ConvertFromBEncode(bEncode);
+                if (torrent != null)
+                {
+                    NewTorrents.Add(torrent);
+                    SelectedNewTorrent = torrent;
+                }
             }
         }
 
         private void ShowAddTorrentWindow()
         {
-            WindowService.ShowWindow<AddTorrentWindow>();
+            WindowService.ShowWindow<AddTorrentsWindow>();
         }
     }
 }
