701c97ca74871fba6665dd087e2a62670c2d14ab
Jesus Fernandez <jfevia@gmail.com>
2017-01-07 16:47:57 +0200

    Extract torrent metadata keys

--- src/Cassiopeia/Converters/TorrentConverter.cs
+++ src/Cassiopeia/Converters/TorrentConverter.cs
@@ -6,74 +6,89 @@ namespace Cassiopeia.Converters
 {
     internal static class TorrentConverter
     {
+        public const string EncodingKey = "encoding";
+        public const string CommentKey = "comment";
+        public const string CreatedByKey = "created by";
+        public const string CreationDateKey = "creation date";
+        public const string AnnounceListKey = "announce-list";
+        public const string AnnounceKey = "announce";
+        public const string NameKey = "name";
+        public const string PieceLengthKey = "piece length";
+        public const string InfoDictionaryKey = "info";
+        public const string PrivateKey = "private";
+        public const string FileListKey = "files";
+        public const string LengthKey = "length";
+        public const string Md5SumKey = "md5sum";
+        public const string PathKey = "path";
+
         public static Torrent ConvertFromBEncode(BEncodedValue value)
         {
             if (value == null)
                 throw new ArgumentNullException(nameof(value));
-            
+
             if (!(value is BEncodedDictionary))
                 throw new ArgumentException($"Invalid BEncode object. Expected root {nameof(BEncodedDictionary)} value");
 
             var torrent = new Torrent();
             var dictionary = (BEncodedDictionary) value;
 
-            if (dictionary.ContainsKey("encoding"))
-                torrent.Encoding = ((BEncodedString) dictionary["encoding"]).Text;
+            if (dictionary.ContainsKey(EncodingKey))
+                torrent.Encoding = ((BEncodedString) dictionary[EncodingKey]).Text;
 
-            if (dictionary.ContainsKey("comment"))
-                torrent.Comment = ((BEncodedString) dictionary["comment"]).Text;
+            if (dictionary.ContainsKey(CommentKey))
+                torrent.Comment = ((BEncodedString) dictionary[CommentKey]).Text;
 
-            if (dictionary.ContainsKey("created by"))
-                torrent.CreatedBy = ((BEncodedString) dictionary["created by"]).Text;
+            if (dictionary.ContainsKey(CreatedByKey))
+                torrent.CreatedBy = ((BEncodedString) dictionary[CreatedByKey]).Text;
 
-            if (dictionary.ContainsKey("creation date"))
-                torrent.CreationDate = ((BEncodedNumber) dictionary["creation date"]).Number;
+            if (dictionary.ContainsKey(CreationDateKey))
+                torrent.CreationDate = ((BEncodedNumber) dictionary[CreationDateKey]).Number;
 
-            if (dictionary.ContainsKey("announce-list"))
+            if (dictionary.ContainsKey(AnnounceListKey))
             {
-                var announceList = (BEncodedList) dictionary["announce-list"];
+                var announceList = (BEncodedList) dictionary[AnnounceListKey];
                 foreach (var announceItem in announceList)
                 {
                     var announceItemList = (BEncodedList) announceItem;
 
                     foreach (var announce in announceItemList)
-                        torrent.AddTracker(new Tracker(((BEncodedString)announce).Text));
+                        torrent.AddTracker(new Tracker(((BEncodedString) announce).Text));
                 }
             }
 
-            if (dictionary.ContainsKey("announce"))
-                torrent.AddTracker(new Tracker(((BEncodedString)dictionary["announce"]).Text));
+            if (dictionary.ContainsKey(AnnounceKey))
+                torrent.AddTracker(new Tracker(((BEncodedString) dictionary[AnnounceKey]).Text));
 
-            if (dictionary.ContainsKey("info"))
+            if (dictionary.ContainsKey(InfoDictionaryKey))
             {
-                var infoDictionary = (BEncodedDictionary) dictionary["info"];
+                var infoDictionary = (BEncodedDictionary) dictionary[InfoDictionaryKey];
 
-                if (infoDictionary.ContainsKey("name"))
-                    torrent.Name = ((BEncodedString) infoDictionary["name"]).Text;
+                if (infoDictionary.ContainsKey(NameKey))
+                    torrent.Name = ((BEncodedString) infoDictionary[NameKey]).Text;
 
-                if (infoDictionary.ContainsKey("piece length"))
-                    torrent.PieceSize = ((BEncodedNumber) infoDictionary["piece length"]).Number;
+                if (infoDictionary.ContainsKey(PieceLengthKey))
+                    torrent.PieceSize = ((BEncodedNumber) infoDictionary[PieceLengthKey]).Number;
 
-                if (infoDictionary.ContainsKey("private"))
-                    torrent.IsPrivate = ((BEncodedNumber) infoDictionary["private"]).Number != 0;
+                if (infoDictionary.ContainsKey(PrivateKey))
+                    torrent.IsPrivate = ((BEncodedNumber) infoDictionary[PrivateKey]).Number != 0;
 
-                if (infoDictionary.ContainsKey("files"))
+                if (infoDictionary.ContainsKey(FileListKey))
                 {
-                    var fileList = (BEncodedList) infoDictionary["files"];
+                    var fileList = (BEncodedList) infoDictionary[FileListKey];
                     foreach (var fileDetails in fileList)
                     {
                         var file = new FileItem();
                         var fileDetailsDictionary = (BEncodedDictionary) fileDetails;
 
-                        if (fileDetailsDictionary.ContainsKey("length"))
-                            file.Size = ((BEncodedNumber) fileDetailsDictionary["length"]).Number;
+                        if (fileDetailsDictionary.ContainsKey(LengthKey))
+                            file.Size = ((BEncodedNumber) fileDetailsDictionary[LengthKey]).Number;
 
-                        if (fileDetailsDictionary.ContainsKey("md5sum"))
-                            file.Md5Sum = ((BEncodedString) fileDetailsDictionary["md5sum"]).Text;
+                        if (fileDetailsDictionary.ContainsKey(Md5SumKey))
+                            file.Md5Sum = ((BEncodedString) fileDetailsDictionary[Md5SumKey]).Text;
 
-                        if (fileDetailsDictionary.ContainsKey("path"))
+                        if (fileDetailsDictionary.ContainsKey(PathKey))
                         {
-                            var filePaths = (BEncodedList) fileDetailsDictionary["path"];
+                            var filePaths = (BEncodedList) fileDetailsDictionary[PathKey];
 
                             foreach (var path in filePaths)
                                 file.Path.Add(((BEncodedString) path).Text);
